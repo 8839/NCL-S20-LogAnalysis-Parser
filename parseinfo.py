b5c86ce53ccc35cbc9c2732712d4b89ec761b6fa
Patrick <perrinepatrick@gmail.com>
2020-04-17 19:23:13 -0700

    pareses lines and data properly, needs format scheme for file output

--- parseinfo.py
+++ parseinfo.py
@@ -3,12 +3,38 @@ import os
 
 def main():
     f = open("test.txt", "r")
-    test = f.readline()
-    x = test.replace(" | ", ",")
-    x = x.replace("\n", ",")
-    print(x)
+    log = f.readlines()
 
-    test2 = f.readline().strip()
-    print(test2 == "(")
+
+
+    output = []
+    array_pos = -1
+    is_array = False
+    for line_num in range(0, len(log)):
+        line = log[line_num].strip()
+        if is_array:
+            if line is ")":
+                is_array = False
+            else:
+                entry = line.split(" ")
+                id = entry[0].replace("[","").replace("]","")
+                if len(entry) == 3:
+                    data = entry[2] 
+                else:
+                    data = "-"
+                print(id + " " + data)
+                output[array_pos] += line + ","
+        else:
+            if line is "(":
+                is_array = True
+            elif line is "":
+                continue
+            else:
+                output.append(line.replace(" | ", ",") + ",")
+                array_pos += 1
+
+    #print(output)
+
+    return
 
 main()
